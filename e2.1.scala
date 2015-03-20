93e1eb85cddabbea61b154210cd1a0813989dad4
Andreas Blum <blum@dial.de>
2015-03-20 15:17:18 +0100

    Replaced tabs with spaces

--- e2.1.scala
+++ e2.1.scala
@@ -1,11 +1,11 @@
 object Exercise_2_1 {
   def fib(n: Long): Long = {
     @annotation.tailrec
-	def go(a: Long, b: Long, n: Long):Long = {
-	  if(n < 1) 0
-	  else if(n < 2) b
-	  else go(b, a+b, n-1)
-	}	  
-	go(0, 1, n)
+    def go(a: Long, b: Long, n: Long):Long = {
+      if(n < 1) 0
+      else if(n < 2) b
+      else go(b, a+b, n-1)
+    }      
+    go(0, 1, n)
   }  
 }
