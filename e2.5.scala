7ae794584793ad88730af17473f7235fe18bff4c
Andreas Blum <blum@dial.de>
2015-04-09 11:24:23 +0200

    Started with exercise 2.5

---
+++ e2.5.scala
@@ -0,0 +1,12 @@
+/*
+
+Exercise 2.5
+Implement the higher-order function that composes two function.
+
+def compose[A,B,C](f: B => C, g: A => B): A => C
+
+*/
+
+object Exercise_2_5 {
+  def compose[A,B,C](f: B => C, g: A => B): A => C = ???
+}
