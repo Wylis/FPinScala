93ac962ba52eee8ab882db3707f8b916ca87c11e
Andreas Blum <blum@dial.de>
2015-04-09 12:06:02 +0200

    Added Exercise 3.2

---
+++ e3.2.scala
@@ -0,0 +1,15 @@
+/*
+
+Exercise 3.2
+Implement the function tail for removing the forst element of a List. Note that the
+function takes constant time. What are different choices you could make in your
+implementation if the List is Nil? We'll return to this question in the next chapter.
+
+*/
+
+object Exercise_3_2 {
+  def tail(l: List[Any]) = { l match {
+    case Nil => throw new NoSuchElementException
+    case x::xs => xs
+  }}
+}
