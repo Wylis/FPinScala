20a6ee61d20e947d24d3764eff0eb35e8c387588
Andreas Blum <blum@dial.de>
2015-04-09 10:47:28 +0200

    Startet with exercise 2.3

---
+++ e2.3.scala
@@ -0,0 +1,14 @@
+/*
+
+Exercise 2.3
+Let's look at another example, currying, which converts a function f of two arguments
+into a function of one argument that partially applies f. Here again there's only one
+implementation that compiles. Write this implementation.
+
+def curry[A,B,C](f: (A, B) => C): A => (B => C)
+
+*/
+
+object Exercise_2_3 {
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = ???
+}
