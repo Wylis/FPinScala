a22197205a08bc90724035b2245cf0d76933b9c5
Andreas Blum <blum@dial.de>
2015-04-09 09:56:12 +0200

    Added exercise description

--- e2.1.scala
+++ e2.1.scala
@@ -1,3 +1,15 @@
+/*
+
+Exercise 2.1
+Write a recursive function to get the n-th Fibonacci number (http://mng.bz/C29s).
+The first two Fibonacci numbers are 0 and 1. The n-th number is always the sum of the
+previous two - The sequence begins 0, 1, 1, 2, 3, 5. Your definition should use a
+local tail-recursive function.
+
+def feb(n: Int): Int
+
+*/
+
 object Exercise_2_1 {
   def fib(n: Long): Long = {
     @annotation.tailrec
