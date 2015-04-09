34596ea2b3144d101de513970e441573d0d06363
Andreas Blum <blum@dial.de>
2015-04-09 09:56:29 +0200

    Startet with exercise 2.2

---
+++ e2.2.scala
@@ -0,0 +1,13 @@
+/*
+
+Exercise 2.2
+Implement isSorted, which checks whether an Array[A] is sorted according to a
+given comparison function:
+
+def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean
+
+*/
+
+object Exercise_2_1 {
+  
+}
