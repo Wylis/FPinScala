f8b7b0fdca10c134168cca22e9d6d8a4e41432c8
Andreas Blum <blum@dial.de>
2015-04-09 10:19:40 +0200

    Finished exercise 2.2

--- e2.2.scala
+++ e2.2.scala
@@ -8,6 +8,13 @@ def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean
 
 */
 
-object Exercise_2_1 {
-  
+object Exercise_2_2 {
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int): Boolean = {
+      if(n > as.length -2) true
+	  else (ordered(as(n),as(n+1))) && go(n+1)
+    }
+	go(0)    
+  }
 }
